f4e14a201b1d6c947d3a64298f039db47edc8047
zachfaughn <zachfaughn@gmail.com>
2025-01-05 18:35:32 -0600

    started parser

---
+++ src/main.py
@@ -0,0 +1,6 @@
+from textnode import TextNode, TextType
+
+def main():
+    print(TextNode("Hello World", TextType.BOLD_TEXT))
+
+main()
---
+++ src/textnode.py
@@ -0,0 +1,20 @@
+from enum import Enum
+
+class TextType(Enum):
+    NORMAL_TEXT = "normal"
+    BOLD_TEXT = "bold"
+    ITALIC_TEXT = "italic"
+    CODE_TEXT = "code"
+    LINKS = "links"
+    IMAGES = "images" 
+
+class TextNode:
+    def __init__(self, text, text_type, url=None):
+        self.text = text
+        self.text_type = text_type
+        self.url = url
+    
+    def __eq__(self, other):
+        return self.text == other.text and self.text_type.value == other.text_type.value and self.url == other.url
+    def __repr__(self):
+        return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
